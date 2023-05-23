fed26ac0311975b4130818f7835d2931a46b87c8
shwet-mishra <shwet.mishra@freshworks.com>
2023-05-23 20:34:43 +0530

    changes for ood

--- finetune/adapter.py
+++ finetune/adapter.py
@@ -40,14 +40,14 @@ devices = 1
 
 # Hyperparameters
 learning_rate = 9e-3
-batch_size = 32 / devices
+batch_size = 64 / devices
 micro_batch_size = 4
 gradient_accumulation_steps = batch_size // micro_batch_size
-epoch_size = 1188  # train dataset size
+epoch_size = 745  # train dataset size
 num_epochs = 5
 max_iters = num_epochs * epoch_size // devices
 weight_decay = 0.02
-max_seq_length = 500  # see scripts/prepare_alpaca.py
+max_seq_length = 256  # see scripts/prepare_alpaca.py
 warmup_steps = epoch_size * 2 // micro_batch_size // devices  # 2 epoch
 
 ds_config = {
