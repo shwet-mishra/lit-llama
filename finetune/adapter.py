02fe851fd3a52fbe68dd22d4fc510c450da05e6d
shwet-mishra <shwet.mishra@freshworks.com>
2023-05-31 17:44:39 +0530

    max tokens changed

--- finetune/adapter.py
+++ finetune/adapter.py
@@ -47,7 +47,7 @@ epoch_size = 1188  # train dataset size
 num_epochs = 5
 max_iters = num_epochs * epoch_size // devices
 weight_decay = 0.02
-max_seq_length = 1536  # see scripts/prepare_alpaca.py
+max_seq_length = 1024  # see scripts/prepare_alpaca.py
 warmup_steps = epoch_size * 2 // micro_batch_size // devices  # 2 epoch
 
 ds_config = {
